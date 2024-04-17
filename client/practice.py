d668bd2365853b8428d99bc51cadbbce734d4d48
sandeep-reddy007 <sandeepreddyaai415@gmail.com>
2024-04-17 12:16:31 +0530

    first_commit

---
+++ client/practice.py
@@ -0,0 +1,5 @@
+import pandas as pandas
+import pandas as pd
+data={'name':['smith','allen','miller'],'Height':[5.1,5.9,5.0],'Qualifaction':['Actor','singer','Businesmen']}
+df=pd.dataFrame(data)
+print(df)
